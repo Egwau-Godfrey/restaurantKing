0a98b7b173d9dc1ad1ae508fd23e544dcf79e9fe
Egwau Godfrey <egwaujunior@gmail.com>
2024-09-17 15:47:20 +0300

    Initial Commit

---
+++ restaurantKing/restaurantKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  restaurantKingApp.swift
+//  restaurantKing
+//
+//  Created by Egwau Godfrey on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct restaurantKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ restaurantKingTests/restaurantKingTests.swift
@@ -0,0 +1,17 @@
+//
+//  restaurantKingTests.swift
+//  restaurantKingTests
+//
+//  Created by Egwau Godfrey on 17/09/2024.
+//
+
+import Testing
+@testable import restaurantKing
+
+struct restaurantKingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
